65964d66680a6580ce30f2f39f2ffe393db8144f
ShntrIto <ito.shintaro.r2@dc.tohoku.ac.jp>
2024-05-30 21:49:32 +0900

    Fix point_to_equirect

--- cuda_rasterizer/auxiliary.h
+++ cuda_rasterizer/auxiliary.h
@@ -46,7 +46,7 @@ __forceinline__ __device__ float ndc2Pix(float v, int S)
 // use n2p_spherical for rendering ERP image
 __forceinline__ __device__ float s2p_spherical(float v, int S)
 {
-	return ((v + 1.0) * S ) * 0.5;
+	return (v + 1.0) * S  * 0.5;
 }
 
 __forceinline__ __device__ void getRect(const float2 p, int max_radius, uint2& rect_min, uint2& rect_max, dim3 grid)
@@ -109,7 +109,7 @@ __forceinline__ __device__ float3 point_to_equirect(
 	float3 view_pos = transformPoint4x3(p_orig, viewmatrix);
 	float view_pos_length = sqrtf(view_pos.x * view_pos.x + view_pos.y * view_pos.y + view_pos.z * view_pos.z);
 	float longitude = atan2f(view_pos.x, view_pos.z);
-	float latitude = asinf(view_pos.y / view_length);
+	float latitude = asinf(view_pos.y / view_pos_length);
 	float screen_y = 2 * latitude / M_PI;
 	float screen_x = longitude / M_PI;
 	float3 p_screen = {screen_x, screen_y, view_pos_length};
